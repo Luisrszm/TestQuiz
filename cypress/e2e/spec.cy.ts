76e58101e41d47125247fd486cdd3ec9b94c0d1a
Luis Rszm <luisrszm@MacBook-Air-de-Luis.local>
2025-06-12 20:04:24 -0600

    edited e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,39 @@
 describe('Homepage', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3001/')
-  })
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:3001/');
+    cy.intercept('GET', '/api/questions/random', {
+      statusCode: 200,
+      body: [
+        {
+          question: 'What is the capital of France?',
+          answers: [
+            { text: 'Paris', isCorrect: true },
+            { text: 'London', isCorrect: false },
+            { text: 'Madrid', isCorrect: false },
+            { text: 'Berlin', isCorrect: false },
+          ],
+        },
+      ],
+      delay: 200
+    }).as('getQuestions');
+  });
+
+  it('permite iniciar el quiz, responder y ver el resultado', () => {
+    cy.contains('Start Quiz').click();
+
+    cy.contains('What is the capital of France?').should('exist');
+
+    cy.contains('.alert', 'Paris').prev('button').click();
+
+    cy.contains('Quiz Completed').should('exist');
+    cy.contains('Your score: 1/1').should('exist');
+
+    cy.contains('Take New Quiz').click();
+    cy.contains('What is the capital of France?').should('exist');
+
+    cy.contains('.alert', 'Paris').prev('button').click();
+
+    cy.contains('Quiz Completed').should('exist');
+    cy.contains('Your score: 1/1').should('exist');
+  });
 })
