0b2a2483a81174bbe8966f8308aee9b1a014100d
Luis Rszm <luisrszm@MacBook-Air-de-Luis.local>
2025-06-12 20:19:26 -0600

    edited unit test

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -2,8 +2,51 @@ import React from 'react'
 import Quiz from '../../client/src/components/Quiz'
 
 describe('<Quiz />', () => {
-  it('Should render the Quiz component', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Quiz />)
-  })
+  const mockQuestions = [
+    {
+      question: 'What is the capital of France?',
+      answers: [
+        { text: 'Paris', isCorrect: true },
+        { text: 'London', isCorrect: false },
+        { text: 'Madrid', isCorrect: false },
+        { text: 'Berlin', isCorrect: false },
+      ],
+    },
+  ];
+
+  beforeEach(() => {
+    cy.intercept('GET', '/api/questions/random', {
+      statusCode: 200,
+      body: mockQuestions,
+      delay: 200
+    }).as('getQuestions');
+  });
+
+  it('should show questions after clicking start', () => {
+    cy.mount(<Quiz />);
+    cy.contains('Start Quiz').click();
+
+    cy.contains(mockQuestions[0].question, { timeout: 10000 }).should('exist');
+
+    mockQuestions[0].answers.forEach(answer => {
+      cy.contains('.alert', answer.text).should('exist');
+    });
+  });
+
+  it('should complete quiz with correct score', () => {
+    cy.mount(<Quiz />);
+    cy.contains('Start Quiz').click();
+
+    cy.contains(mockQuestions[0].question, { timeout: 10000 }).should('exist');
+
+    const correctAnswer = mockQuestions[0].answers.find(a => a.isCorrect);
+    if (correctAnswer) {
+      cy.contains('.alert', correctAnswer.text)
+        .prev('button')
+        .click();
+    }
+
+    cy.contains('Quiz Completed').should('exist');
+    cy.contains(`Your score: 1/${mockQuestions.length}`).should('exist');
+  });
 })
